eed559133868815267c789abcedcb9980907ab78
tfkhdyt <ta142fa@gmail.com>
2021-08-02 10:08:17 +0700

    ganti port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const {
   loadTanaman,
